d46c3974af4600539b8c03a054e351a06afd0b45
Figeral <mouliofitzgerard@gmail.com>
2024-05-28 12:05:53 +0100

    project set up minimal dependency added addition of static files addition of fonts

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final dangerButton = Colors.red[600];
+  static const cancelButton = Colors.white;
+  static const tapButton = Color(0x754fb0);
+  static const lightPurple = Color(0x8714b7);
+  static const deepPurple = Color(0x7041c1);
+}
